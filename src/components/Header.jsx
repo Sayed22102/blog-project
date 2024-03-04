a0dcc0ebfc44e216d019ec0454c23f3ba5534d77
Sayed22102 <160048693+Sayed22102@users.noreply.github.com>
2024-03-04 15:33:51 +0000

    save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,66 +1,148 @@
-import React, { useState } from 'react'
-import { AiOutlineMenu ,AiOutlineClose } from "react-icons/ai";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import React, { useState } from "react";
 
-import { images } from '../constants';
-const navItemInfo=[
-  { name: "Home", type: "link", href: "/" },
-  { name: "Blog", type: "link", href: "/blog" },
-  {
-    name: "Pages",
-    type: "dropdown",
-    items: [
-      { title: "About us", href: "/about" },
-      { title: "Contact us", href: "/contact" },
-    ],
-  },
-  { name: "Pricing", type: "link", href: "/pricing" },
-  { name: "Faq", type: "link", href: "/faq" },
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
+
+import { IoIosArrowDown } from "react-icons/io";
+
+import { images } from "../constants";
+
+const navItemsInfo = [
+
+  { name: "Home", type: "link" },
+
+  { name: "Articles", type: "link" },
+
+  { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
+
+  { name: "Pricing", type: "link" },
+
+  { name: "Faq", type: "link" },
+
 ];
 
+const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
 
-const NavItem = ({name})=>{
-  return(
-    <li className=' relative group'>
-              <a href='/' className='px-4 py-2 '>
-                {name}</a>
-              <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100"></span>
-            </li>
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => {
+      return !curState;
+    });
+  };
+
+  return (
+    <li className="relative group">
+      {item.type === "link" ? (
+        <>
+          <Link to={item.href} className="px-4 py-2">
+            {item.name}
+          </Link>
+          <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <div className="flex flex-col items-center">
+          <button
+            className="px-4 py-2 flex gap-x-1 items-center"
+            onClick={toggleDropdownHandler}
+          >
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
+          </button>
+          <div
+            className={`${
+              dropdown ? "block" : "hidden"
+            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+          >
+            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page, index) => (
+                <Link
+                  key={index}
+                  to={page.href}
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                >
+                  {page.title}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </li>
   );
-}
+};
+
 const Header = () => {
-  const[navIsVisible, setNavIsVisible] = useState(false);
+
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
   const navVisibilityHandler = () => {
     setNavIsVisible((curState) => {
       return !curState;
     });
+  };
+
+
   return (
+
     <section>
-      <Header className="container mx-auto px-5 flex justify-between py-4">
+
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+
         <div>
-          <img src={images.Logo} alt="logo"/>
-        
+
+          <img className="w-16" src={images.Logo} alt="Logo" />
+
         </div>
-        <div className="lg:hidden z-50"> 
-          {navIsVisible ? (<AiOutlineClose className='w-6 h-6' onClick={navVisibilityHandler} /> ):(
-          <AiOutlineMenu className='w-6 h-6' onClick={navVisibilityHandler}/> )}
+
+        <div className=" lg:hidden z-50">
+
+          {navIsVisible ? (
+
+            <IoCloseSharp className="w-6 h-6" onClick={navVisibilityHandler} />
+
+          ) : (
+
+            <IoMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+
+          )}
+
         </div>
+
         <div
-        className={`${
+
+          className={`${
+
             navIsVisible ? "right-0" : "-right-full"
-          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}>
-         
+
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+
+          >
+
           <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
-           {navItemInfo.map((item)=> (
-            <NavItem key={item.name} item={item}/>
-           ))}
+
+            {navItemsInfo.map((item) => (
+
+              <NavItem key={item.name} item={item} />
+
+            ))}
+
           </ul>
-          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300" >
-            Sign in</button>
-            </div>
-      </Header>
+
+          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+
+            Sign in
+
+          </button>
+
+        </div>
+
+      </header>
+
     </section>
+
   );
+
 };
 
 export default Header;
