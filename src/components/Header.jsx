9bc48721f31eb71bb47f5dd235c05e569cc772ec
Sayed22102 <160048693+Sayed22102@users.noreply.github.com>
2024-03-06 07:28:05 +0000

    part6 v6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-
-import { IoMenu, IoCloseSharp } from "react-icons/io5";
-
-import { IoIosArrowDown } from "react-icons/io";
-
+import React, { useState, } from "react";
 import { images } from "../constants";
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const navItemsInfo = [
 
@@ -33,9 +30,9 @@ const NavItem = ({ item }) => {
     <li className="relative group">
       {item.type === "link" ? (
         <>
-          <Link to={item.href} className="px-4 py-2">
+          <a to={item.href} className="px-4 py-2">
             {item.name}
-          </Link>
+          </a>
           <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
             /
           </span>
@@ -55,15 +52,6 @@ const NavItem = ({ item }) => {
             } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
           >
             <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
-              {item.items.map((page, index) => (
-                <Link
-                  key={index}
-                  to={page.href}
-                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
-                >
-                  {page.title}
-                </Link>
-              ))}
             </ul>
           </div>
         </div>
